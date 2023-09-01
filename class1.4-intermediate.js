95d7d35bbfd6fa945f5e38b521bf3564bcd71491
Boya Zhang <boyazhang@Boyas-Air.attlocal.net>
2023-08-31 20:41:29 -0400

    completed course

---
+++ class1.4-intermediate.js
@@ -0,0 +1,20 @@
+//since time at 5:00:00 
+//start all js to manipulate the DOM and get it value and styles
+
+
+//1.0 Module- it's a file of reusable codes
+//we can import sections of pre-written code to use whenever we want
+//Great for any general utility values and functions
+//Helps to make your code reusable and maintainable
+// export const PI = 3.1415926;
+// export function getCircumference(radius) {
+//     return 2 * PI * radius;
+// }
+//import {PI, getCircumference} from 'class1.4-intermediate.js'
+//import * as myName from ''
+
+//2.0 DOM = Document Object Model(API)document <html><head><body>
+//An interface for changing the content of a page
+//console.dir(document);
+//console.log(document.title);.URL/.location
+// //5 hours - use js to control the DOMs
